35bf07ce8c8c43e0d7d2fa1db90116f431014e58
Shan-droid770 <kumaradissanayaka30@gmail.com>
2025-11-06 21:20:53 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,7 +2,7 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Sahan Dissanyaka', // Your name
+  botOwner: 'Sahan Dissanayaka', // Your name
   ownerNumber: '94763952882', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
